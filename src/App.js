bce97d3157347a8c76bfa49f49c8e345977dfd74
Hanszman <victor.hanszman@hotmail.com>
2024-07-20 11:06:44 -0300

    continuação da criação do chat

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
+import { useState } from 'react';
 import io from 'socket.io-client';
+import './App.css';
 
 const socket = io.connect('http://localhost:3001');
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [room, setRoom] = useState('');
+  const joinRoom = () => {
+    if (username && room) {
+      
+    }
+  }
   return (
-    <div className="App"></div>
+    <div className="App">
+      <h3>Joian A Chat</h3>
+      <input
+        type="text"
+        placeholder="John..."
+        onChange={event => {setUsername(event.target.value)}}
+      />
+      <input
+        type="text"
+        placeholder="Room ID..."
+        onChange={event => {setRoom(event.target.value)}}
+      />
+      <button>Join A Room</button>
+    </div>
   );
 }
 
